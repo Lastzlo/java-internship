60a8679c8f9bcd69253c9b29a601cdd3a448ec81
Lastzlo <z.last.zlo@gmail.com>
2021-09-06 14:58:52 +0300

    feat add annotations NewName.java, Ignore.java

---
+++ home-work-3/practiceTasks/src/main/java/task1_ObjectMapper/util/Ignore.java
@@ -0,0 +1,11 @@
+package task1_ObjectMapper.util;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+@interface Ignore {
+}
---
+++ home-work-3/practiceTasks/src/main/java/task1_ObjectMapper/util/NewName.java
@@ -0,0 +1,12 @@
+package task1_ObjectMapper.util;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
+@interface NewName {
+    String name();
+}
