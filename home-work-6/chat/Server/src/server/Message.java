88e5132ae888196e02bb7c1afa178990247206dc
Lastzlo <z.last.zlo@gmail.com>
2021-09-16 16:52:47 +0300

    refactor change time format

--- home-work-6/chat/Server/src/server/Message.java
+++ home-work-6/chat/Server/src/server/Message.java
@@ -9,13 +9,15 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.nio.charset.StandardCharsets;
 import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
 
 public class Message {
-	private LocalTime date = LocalTime.now();
+	private String time;
 	private String from;
 	private String text;
 
 	public Message(String from, String text) {
+		this.time = LocalTime.now().format(DateTimeFormatter.ofPattern("HH:mm"));
 		this.from = from;
 		this.text = text;
 	}
@@ -24,28 +26,28 @@ public class Message {
 		Gson gson = new GsonBuilder().create();
 		return gson.toJson(this);
 	}
-	
+
 	public static Message fromJSON(String s) {
 		Gson gson = new GsonBuilder().create();
 		return gson.fromJson(s, Message.class);
 	}
-	
+
 	@Override
 	public String toString() {
-		return new StringBuilder().append("[").append(date)
+		return new StringBuilder().append("[").append(time)
 				.append(", From: ").append(from)
 				.append("] ").append(text)
-                .toString();
+				.toString();
 	}
 
 	public int send(String url) throws IOException {
 		URL obj = new URL(url);
 		HttpURLConnection conn = (HttpURLConnection)
 				obj.openConnection();
-		
+
 		conn.setRequestMethod("POST");
 		conn.setDoOutput(true);
-	
+
 		OutputStream os = conn.getOutputStream();
 		try {
 			String json = toJSON();
