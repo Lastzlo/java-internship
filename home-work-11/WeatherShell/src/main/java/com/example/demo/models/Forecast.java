a707f1a4732b35e4b99bdc588f03cca18b692471
Lastzlo <z.last.zlo@gmail.com>
2021-09-30 03:37:43 +0300

    feat impl toString

--- home-work-11/WeatherShell/src/main/java/com/example/demo/models/Forecast.java
+++ home-work-11/WeatherShell/src/main/java/com/example/demo/models/Forecast.java
@@ -2,11 +2,20 @@ package com.example.demo.models;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import java.util.Arrays;
+
 public class Forecast {
     @JsonProperty("consolidated_weather")
     private WeatherOnDay consolidatedWeather[];
 
     public WeatherOnDay[] getConsolidatedWeather() {
         return consolidatedWeather;
+    }
+
+    @Override
+    public String toString() {
+        return "Forecast{" +
+                "consolidatedWeather=" + Arrays.toString(consolidatedWeather) +
+                '}';
     }
 }
